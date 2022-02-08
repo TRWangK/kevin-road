b8efcdcc5eec348c2e90f286d158d3d6e2ce2aec
wangtianrui01 <wangtianrui01@baijia.com>
2022-02-08 16:38:14 +0800

    CalculateMoney 2

--- test/src/main/java/go.kevin/CalculateMoney.java
+++ test/src/main/java/go.kevin/CalculateMoney.java
@@ -2,8 +2,8 @@ package go.kevin;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author Tianrui Wang
@@ -12,29 +12,49 @@ import java.util.Map;
 public class CalculateMoney {
 	public static void main(String[] args) {
 
-		int age = 26;
-		int base = 80000;
+		int age = 27;
+		int base = 150000;
 
-		Map<Integer, Integer> investAmountMapByYear = new HashMap<>();
-		investAmountMapByYear.put(2021, 100000);
-		investAmountMapByYear.put(2022, 200000);
-		investAmountMapByYear.put(2023, 200000);
-		investAmountMapByYear.put(2024, 300000);
-		investAmountMapByYear.put(2025, 120000);
-		investAmountMapByYear.put(2026, 120000);
+		Map<Integer, Integer> investAmountMapByYear = new TreeMap<Integer, Integer>();
+		investAmountMapByYear.put(2022, 250000);
+		investAmountMapByYear.put(2023, 300000);
+		investAmountMapByYear.put(2024, 100000);
+		investAmountMapByYear.put(2025, 100000);
+		investAmountMapByYear.put(2026, 100000);
 		investAmountMapByYear.put(2027, 150000);
 		investAmountMapByYear.put(2028, 150000);
 		investAmountMapByYear.put(2029, 150000);
-//		investAmountMapByYear.put(2030, 150000);
-//		investAmountMapByYear.put(2031, 150000);
-//		investAmountMapByYear.put(2032, 150000);
-//		investAmountMapByYear.put(2033, 150000);
-//		investAmountMapByYear.put(2034, 150000);
+		investAmountMapByYear.put(2030, 150000);
+		investAmountMapByYear.put(2031, 150000);
+		investAmountMapByYear.put(2032, 150000);
+		investAmountMapByYear.put(2033, 150000);
+		investAmountMapByYear.put(2034, 150000);
+		investAmountMapByYear.put(2035, 100000);
+		investAmountMapByYear.put(2036, 100000);
+		investAmountMapByYear.put(2037, 100000);
+		investAmountMapByYear.put(2038, 100000);
+		investAmountMapByYear.put(2039, 100000);
+		investAmountMapByYear.put(2040, 100000);
+		investAmountMapByYear.put(2041, 0);
+		investAmountMapByYear.put(2042, 0);
+		investAmountMapByYear.put(2043, 0);
+		investAmountMapByYear.put(2044, 0);
+		investAmountMapByYear.put(2045, 0);
+		investAmountMapByYear.put(2046, -100000);
+		investAmountMapByYear.put(2047, -100000);
+		investAmountMapByYear.put(2048, -100000);
+		investAmountMapByYear.put(2049, -100000);
+		investAmountMapByYear.put(2050, -100000);
+		investAmountMapByYear.put(2051, -200000);
+		investAmountMapByYear.put(2052, -200000);
+		investAmountMapByYear.put(2053, -200000);
+		investAmountMapByYear.put(2054, -200000);
+		investAmountMapByYear.put(2055, -200000);
 
-		double profitRatio = 0.12D;
+		double profitRatio = 0.1D;
 
-		int housePrice = 1300000;
-		int marriagePrice = 200000;
+		int housePrice = 1500000;
+		int marriagePrice = 120000;
 
 		calculate(age, base, investAmountMapByYear, profitRatio, housePrice, marriagePrice);
 	}
@@ -63,19 +83,19 @@ public class CalculateMoney {
 
 			double profit = (total + (investAmount / 2D)) * profitRatio;
 			total = total + investAmount + profit;
-			System.out.println(StringUtils.join(year, "年: 年底", age, "岁, 年终总金额: ", total, ", 新投资金额: ", investAmount, ", 收益: ", profit));
+			System.out.println(StringUtils.join(year, "年: 年底", age, "岁, 年终总金额: ", (int)total, ", 新投资金额: ", investAmount, ", 收益: ", (int)profit));
 
 			// 买房事件
-			if (total > houseDownPrice && houseLoan == 0){
+			if (age >= 30 && total > houseDownPrice && houseLoan == 0){
 				total -= houseDownPrice;
 				houseLoan += houseDownPrice * 0.01 * 12;
-				System.out.println(StringUtils.join("--- 买房, 首付扣除本金: ", houseDownPrice, ", 每年扣除投资金额: ", houseLoan));
+				System.out.println(StringUtils.join("--- 买房, 首付扣除本金: ", houseDownPrice, ", 余额: ", (int)total, ", 每年扣除投资金额: ", houseLoan));
 			}
 
 			// 结婚事件
-			if (age == 30){
+			if (age == 29){
 				total -= marriagePrice;
-				System.out.println(StringUtils.join("--- 结婚, 扣除本金: ", marriagePrice));
+				System.out.println(StringUtils.join("--- 结婚, 扣除本金: ", marriagePrice, ", 余额: ", (int)total));
 			}
 
 		}
